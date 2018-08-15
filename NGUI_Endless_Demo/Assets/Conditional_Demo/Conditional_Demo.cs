5b605d2261cfd80e34741ea49b5bde1647072eb5
FangyeReady <37165236+FangyeReady@users.noreply.github.com>
2018-08-15 14:34:32 +0800

    :+1:

---
+++ NGUI_Endless_Demo/Assets/Conditional_Demo/Conditional_Demo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Conditional_Demo : MonoBehaviour {
+
+    public GUIContent content;
+    private void Start()
+    {
+        
+    }
+
+    [Conditional("CAN_USE")]
+    void FuncA()
+    {
+        Utilty.Log("can use");
+    }
+
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(20,20, 250, 100), content))
+        {
+            FuncA();
+        }
+    }
+}
---
+++ NGUI_Endless_Demo/Assets/Utilty.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+
+
+public static class Utilty {
+ 
+    [Conditional("UNITY_EDITOR")]
+    public static void Log(string str)
+    {
+       UnityEngine.Debug.Log("[Log] " + str);
+    }
+}
