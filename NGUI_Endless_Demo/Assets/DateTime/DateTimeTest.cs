92349e763246a19b665a120182392f143febc6d1
FangyeReady <37165236+FangyeReady@users.noreply.github.com>
2018-11-01 18:30:09 +0800

    。。。。

--- NGUI_Endless_Demo/Assets/DateTime/DateTimeTest.cs
+++ NGUI_Endless_Demo/Assets/DateTime/DateTimeTest.cs
@@ -5,19 +5,20 @@ using System;
 
 public class DateTimeTest : MonoBehaviour {
 
-    private readonly DateTime old = new DateTime(2018, 11, 1);
+    private readonly DateTime old = new DateTime(2018, 12, 30);
 	// Use this for initialization
 	void Start () {
-        int off = CalcDaysDiff(old, DateTime.Now);
-
+        //first.
+        int off = CalcDaysDiff(DateTime.Now, old);
         Debug.Log(off.ToString());
+
+        //second.
         TimeSpan days = old - DateTime.Now;
         Debug.Log(days.Days.ToString());
 
+        //add.
         DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1, 0, 0, 0));
         var date = startTime.AddSeconds(1540458219);
-
-
         Debug.LogError(date.ToString());
 	}
 
