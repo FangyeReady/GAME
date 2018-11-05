6f89a8398c52d39bee4e90bec5f1b1b6f07b2499
FangyeReady <37165236+FangyeReady@users.noreply.github.com>
2018-11-05 15:37:00 +0800

    :+1:

---
+++ New Unity Project/Assets/Scripts/Frame/ImageRotate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageRotate : MonoBehaviour {
+
+    private Image img;
+    private float angle = 0.0f;
+
+    [Range(0.1f, 1)]
+    public float speed = 0.1f;
+	
+	void Start () {
+
+        img = this.GetComponent<Image>();
+	}
+	
+	void Update () {
+
+        angle += Time.deltaTime * speed;
+        this.gameObject.transform.Rotate(Vector3.forward, angle);
+
+	}
+}
---
+++ New Unity Project/Assets/Scripts/Frame/SetMTexOffset.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetMTexOffset : MonoBehaviour {
+
+    private Image img;
+
+	void Start () {
+        img = this.GetComponent<Image>();
+ 
+	}
+
+    float ofx = 0.0f;
+    float ofy = 0.0f;
+    void Update () {
+
+        ofx += Time.deltaTime * 0.15f;
+        ofy += Time.deltaTime * 0.15f;
+        img.materialForRendering.SetTextureOffset("_MainTex", new Vector2(ofx, ofy));
+
+    }
+}
