78cc7a808692d70f032f2d97d05abddc19560f7b
FangYe <37165236+FangyeReady@users.noreply.github.com>
2019-04-17 16:23:37 +0800

    面试代码

--- New Unity Project/Assets/Scripts/UI/PopupServentHome.cs
+++ New Unity Project/Assets/Scripts/UI/PopupServentHome.cs
@@ -5,37 +5,37 @@ using UnityEngine.UI;
 
 public class PopupServentHome : PopupBase {
 
-    public Transform headParent;
-    public GameObject headItem;
+    public Transform HeadParent;
+    public GameObject HeadItem;
 
-    public Transform cellParent;
-    public GameObject cellPrefab;
-    public GameObject propPrefab;
+    public Transform CellParent;
+    public GameObject CellPrefab;
+    public GameObject PropPrefab;
 
     [Header("UI Info")]
-    public Image headPic;
+    public Image HeadPic;
     public Text Name;
     public Text SkillDesc;
     public Text Level;
     public Text Loyal;
-    public Text expNow;
-    public Text expAll;
-    public Slider expSlider;
+    public Text ExpNow;
+    public Text ExpAll;
+    public Slider ExpSlider;
 
     //从配置表里面读
     [Header("Flag Ser Info")]
-    public Text serAll;
-    public Text serHave;
-    public Text tire;
-    public Text favorability;
+    public Text SerAll;
+    public Text SerHave;
+    public Text Tire;
+    public Text Favorability;
 
     [Header("下拉列表")]
-    public Dropdown dropdown;
+    public Dropdown Dropdown;
 
-    private List<GameObject> masks = new List<GameObject>();
-    private List<ServentHeadItem> heads = new List<ServentHeadItem>();
-    private List<GameObject> gameObjects = new List<GameObject>();
-    private List<PropItem> propItems = new List<PropItem>();
+    private List<GameObject> masks_list = new List<GameObject>();
+    private List<ServentHeadItem> heads_list = new List<ServentHeadItem>();
+    private List<GameObject> gameObjects_list = new List<GameObject>();
+    private List<PropItem> propItems_list = new List<PropItem>();
 
     private ServentInfo curServent;
     public ServentInfo CurServentInfo {
@@ -49,15 +49,15 @@ public class PopupServentHome : PopupBase {
         base.Init();
         CreateItems();
 
-        dropdown.onValueChanged.AddListener(SetValue);
-        SetValue(dropdown.value);
+        Dropdown.onValueChanged.AddListener(SetValue);
+        SetValue(Dropdown.value);
     }
 
     private void CreateItems()
     {
         CreateHeadItems();
         CreateCellItems();
-        CreatePropItems();
+        CreatePropItemsList();
 
         //StartCoroutine("UpdateServentLevel", 12000);
     }
@@ -67,15 +67,15 @@ public class PopupServentHome : PopupBase {
         var list = Player.Instance.PlayerInfos.Servent;
         for (int i = 0; i < list.Count; i++)
         {
-            GameObject obj = Instantiate(headItem, headParent);
+            GameObject obj = Instantiate(HeadItem, HeadParent);
             obj.SetActive(true);
             obj.name = i.ToString();
-            gameObjects.Add(obj);
+            gameObjects_list.Add(obj);
 
             ServentHeadItem item = obj.GetComponent<ServentHeadItem>();
             item.Init(list[i]);
-            heads.Add(item);
-            masks.Add(item.mask);
+            heads_list.Add(item);
+            masks_list.Add(item.mask);
         }
 
         RefreshChooseState(0, list[0].ID);
@@ -87,13 +87,13 @@ public class PopupServentHome : PopupBase {
         int count = GameManager.Instance.GameSettingInfos.cellCount;
         for (int i = 0; i < count; i++)
         {
-            var obj = Instantiate(cellPrefab, cellParent);
+            var obj = Instantiate(CellPrefab, CellParent);
             obj.SetActive(true);
             cellList.Add(obj.transform);
         }
     }
 
-    private void CreatePropItems()
+    private void CreatePropItemsList()
     {
         //to do..
         var list = Player.Instance.PlayerInfos.propID;
@@ -106,14 +106,14 @@ public class PopupServentHome : PopupBase {
             Transform parent = GetEmptyCell();
             if (parent != null)
             {
-                var item = Instantiate(propPrefab, parent);
+                var item = Instantiate(PropPrefab, parent);
                 item.SetActive(true);
                 item.transform.localScale = Vector3.one;
                 item.transform.localPosition = Vector3.zero;
                 PropItem info = item.GetComponent<PropItem>();
                 info.InitProp(list[i].id, list[i].num);
                 info.SetTargetServent(curServent);
-                propItems.Add(info);
+                propItems_list.Add(info);
             } 
         }
     }
@@ -133,9 +133,9 @@ public class PopupServentHome : PopupBase {
 
     public void RefreshChooseState(int index, int serID)
     {
-        for (int i = 0; i < masks.Count; i++)
+        for (int i = 0; i < masks_list.Count; i++)
         {
-            masks[i].SetActive( i == index);
+            masks_list[i].SetActive( i == index);
         }
         ServentInfo servent = Player.Instance.PlayerInfos.Servent.Find(ser=>ser.ID == serID);
         if (servent != null)
@@ -145,15 +145,15 @@ public class PopupServentHome : PopupBase {
         }
 
         //重置道具的使用对象
-        for (int i = 0; i < propItems.Count; i++)
+        for (int i = 0; i < propItems_list.Count; i++)
         {
-            if (propItems[i] != null)
+            if (propItems_list[i] != null)
             {
-                propItems[i].SetTargetServent(curServent);
+                propItems_list[i].SetTargetServent(curServent);
             }
             else
             {
-                propItems.RemoveAt(i);
+                propItems_list.RemoveAt(i);
             }
         }
     }
@@ -163,18 +163,18 @@ public class PopupServentHome : PopupBase {
         Name.text = info.Name;
         Level.text = info.Level.ToString();
         Loyal.text = info.Loyal.ToString();
-        tire.text = info.tire.ToString();
-        favorability.text = info.favorability.ToString();
+        Tire.text = info.Tire.ToString();
+        Favorability.text = info.Favorability.ToString();
         SkillDesc.text = info.Desc.ToString();
-        expNow.text = info.nowexp.ToString();
-        expAll.text = ServentManager.Instance.GetServentNextLevelExp(info.Level).ToString();
+        ExpNow.text = info.nowexp.ToString();
+        ExpAll.text = ServentManager.Instance.GetServentNextLevelExp(info.Level).ToString();
 
-        expSlider.value = (int.Parse(expNow.text) + 0.0f) / int.Parse(expAll.text);
+        ExpSlider.value = (int.Parse(ExpNow.text) + 0.0f) / int.Parse(ExpAll.text);
 
-        serHave.text = Player.Instance.PlayerInfos.Servent.Count.ToString();
-        serAll.text = GameManager.Instance.GetServentCount().ToString();
+        SerHave.text = Player.Instance.PlayerInfos.Servent.Count.ToString();
+        SerAll.text = GameManager.Instance.GetServentCount().ToString();
 
-        ResourcesLoader.Instance.SetSprite(StaticData.HEAD_ICON_PATH, "list" + info.ID.ToString(), sp => headPic.sprite = sp);
+        ResourcesLoader.Instance.SetSprite(StaticData.HEAD_ICON_PATH, "list" + info.ID.ToString(), sp => HeadPic.sprite = sp);
     }
 
     private void SetValue(int val)
@@ -182,7 +182,7 @@ public class PopupServentHome : PopupBase {
         switch (val)
         {
             case 0:
-                heads.Sort((a,b)=> {
+                heads_list.Sort((a,b)=> {
                     int result = a.info.Level.CompareTo(b.info.Level);//根据level排序，result为1则从小到大，-1为从大到小
                     if(result == 0)//为0则相等
                     {
@@ -193,7 +193,7 @@ public class PopupServentHome : PopupBase {
                 SortItems();
                 break;
             case 1:
-                heads.Sort((a, b) =>
+                heads_list.Sort((a, b) =>
                 {
                     int result = a.info.star.CompareTo(b.info.star);//根据level排序，result为1则从小到大，-1为从大到小
                     if (result == 0)//为0则相等
@@ -205,7 +205,7 @@ public class PopupServentHome : PopupBase {
                 SortItems();
                 break;
             case 2:
-                heads.Sort((a, b) =>
+                heads_list.Sort((a, b) =>
                 {
                     int result = a.info.ID.CompareTo(b.info.ID);//根据level排序，result为1则从小到大，-1为从大到小
                     if (result == 0)//为0则相等
@@ -224,13 +224,13 @@ public class PopupServentHome : PopupBase {
     private void SortItems()
     {
         List<ServentInfo> infos = new List<ServentInfo>();
-        for (int i = 0; i < heads.Count; i++)
+        for (int i = 0; i < heads_list.Count; i++)
         {
-            infos.Add(heads[i].info);
+            infos.Add(heads_list[i].info);
         }
-        for (int i = 0; i < gameObjects.Count; i++)
+        for (int i = 0; i < gameObjects_list.Count; i++)
         {
-            var item = gameObjects[i].GetComponent<ServentHeadItem>();
+            var item = gameObjects_list[i].GetComponent<ServentHeadItem>();
             item.Init(infos[i]);
         }
         RefreshChooseState(0, infos[0].ID);
@@ -266,31 +266,31 @@ public class PopupServentHome : PopupBase {
                 offsetExp = nextexp - curServent.nowexp;
                 //           如果大于这个经验，则升级       小于，则在原有基础上加上现有的
                 targetVal = (i - offsetExp >= 0) ? 1f : (i + 0.0f + curServent.nowexp) / nextexp;
-                while (targetVal - expSlider.value >= 0.01f)
+                while (targetVal - ExpSlider.value >= 0.01f)
                 {
                     if (targetVal == 1f)
                     {
-                        expSlider.value += fillSpeed;
+                        ExpSlider.value += fillSpeed;
                         curServent.nowexp += (int)(nextexp * fillSpeed);
                     }
                     else
                     {
-                        expSlider.value = Mathf.Lerp(expSlider.value, targetVal, Time.deltaTime);
-                        curServent.nowexp = (int)(expSlider.value * nextexp);
+                        ExpSlider.value = Mathf.Lerp(ExpSlider.value, targetVal, Time.deltaTime);
+                        curServent.nowexp = (int)(ExpSlider.value * nextexp);
                     }
-                    expNow.text = curServent.nowexp.ToString();
+                    ExpNow.text = curServent.nowexp.ToString();
                     yield return null;
                 }
                 if (targetVal == 1f)
                 {
                     curServent.Level += 1;
-                    expSlider.value = 0.0f;
+                    ExpSlider.value = 0.0f;
                     curServent.nowexp = 0;
-                    expNow.text = "0";
+                    ExpNow.text = "0";
                     nextexp = ServentManager.Instance.GetServentNextLevelExp(curServent.Level);
                     if (nextexp != 0)
                     {
-                        expAll.text = nextexp.ToString();
+                        ExpAll.text = nextexp.ToString();
                     }
                 }
                 Level.text = curServent.Level.ToString();
