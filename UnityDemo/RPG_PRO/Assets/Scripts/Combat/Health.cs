a7248f976e63842f8a5820a8a3e537270d511f3a
FangyeReady <ye.fang@pitayagames.com>
2019-05-22 08:29:49 +0800

    dead ani

--- UnityDemo/RPG_PRO/Assets/Scripts/Combat/Health.cs
+++ UnityDemo/RPG_PRO/Assets/Scripts/Combat/Health.cs
@@ -6,11 +6,29 @@ namespace  RPG.Combat
     public class Health:MonoBehaviour
     {
         [SerializeField] float health = 20;
+
+        private bool isDead = false;
+
         public void TakeDamage( float damage )
         {
-            health = Mathf.Max( health - damage, 0 );
+            health = Mathf.Max(health - damage, 0);
             Debug.Log("Health:" + health);
+
+           if( 0 == health) Die();
         }
 
+        private void Die()
+        {
+            if(isDead) return;
+            
+            isDead = true;
+            GetComponent<Animator>().SetTrigger("die");
+        }
+
+
+
     }
+
+
+    
 }
